11c5e669729cdafe6f627401a8fdfff7adf38bf8
Handi067 <111344381+Handi067@users.noreply.github.com>
2022-11-17 10:38:30 +0700

    Add files via upload
    Tugas dan latihan Pemrograman Web

---
+++ Praktikum Pemrograman Web/Tugas Php/php2.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+                jkj
+        </title>
+    </head>
+    <body>
+        <?php
+        $nama[] = "hussaini";
+        $nama[] = "najib";
+        $nama[] = "hamdi";
+        echo $nama[1] . $nama[2] . $nama[0] ;
+        echo "<br>";
+        ?>
+    </body>
+</html>
---
+++ Praktikum Pemrograman Web/Tugas Php/php4.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+                tanggalan
+        </title>
+    </head>
+    <body>
+        <?php
+      echo date("m-F-Y, g:i:s a");
+        ?>
+    </body>
+</html>
